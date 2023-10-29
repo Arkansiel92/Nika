f43cddcd1277e43559817ca8b6eaccd47a9ed96b
Arkansiel92 <yaj92250@hotmail.fr>
2023-10-29 18:03:19 +0100

    fix IP socket server

--- client/Services/Contexts/Socket/Socket.ts
+++ client/Services/Contexts/Socket/Socket.ts
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 import { io } from "socket.io-client";
 
-export const socket = io('http://127.0.0.1:3001', {transports: ['websocket']});
+export const socket = io('http://192.168.134.56:3001', {transports: ['websocket']});
 
 export const socketContext = createContext(socket);
